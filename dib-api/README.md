7332fd5a793acc4fec38ef3d947f71d9f5b4dc64
Tommi Siik <tsiika9@gmail.com>
2018-05-21 15:40:04 +0300

    It's alive!
    First commit!

---
+++ dib-api/README.md
@@ -0,0 +1,3 @@
+# This is not the README you are looking for!
+
+See the README on the root.
---
+++ dib-front/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <h1 className="App-title">dib</h1>
+        
+        <p className="App-intro">
+        And here we go!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
