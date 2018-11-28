d60f7bcbf045ef322113f3361426c3bebd84e4d8
Tommi Siik <tsiika9@gmail.com>
2018-11-28 16:12:20 +0200

    Authentication revision #2
    - Reworked passport strategy configuration

--- dib-api/config/passport.js
+++ dib-api/config/passport.js
@@ -1,24 +1,25 @@
-const   JwtStrategy = require('passport-jwt').Strategy,
-        ExtractJwt = require('passport-jwt').ExtractJwt;
+const JwtStrategy = require("passport-jwt").Strategy,
+    ExtractJwt = require("passport-jwt").ExtractJwt;
 
 // User model
-var User = require('../models/User');
-var settings = require('./settings'); // Settings file
+var User = require("../models/User");
+var settings = require("./settings"); // Settings file
 
-module.exports = function(passport) {
-    var opts = {}
-    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-    opts.secretOrKey = settings.secret;
-    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-        User.findOne({id: jwt_payload.id}, function(err, user) {
-            if(err) {
-                return done(err, false);
-            }
-            if(user) {
-                done(null, user);
-            } else {
-                done(null, false);
-            }
-        });
-    }));
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = settings.secret;
+
+module.exports = passport => {
+    passport.use(
+        new JwtStrategy(opts, (jwt_payload, done) => {
+            User.findById(jwt_payload.id)
+                .then(user => {
+                    if (user) {
+                        return done(null, user);
+                    }
+                    return done(null, false);
+                })
+                .catch(err => console.log(err));
+        })
+    );
 };
--- dib-api/models/User.js
+++ dib-api/models/User.js
@@ -26,36 +26,6 @@ const UserSchema = new Schema({
     }
 });
 
-/*
-UserSchema.pre('save', function(next) {
-    var user = this;
-    if (this.isModified('password') || this.isNew) {
-        bcrypt.genSalt(10, function(err, salt) {
-            if(err) {
-                return next(err);
-            }
-            bcrypt.hash(user.password, salt, null, function(err, hash) {
-                if (err) {
-                    return next(err);
-                }
-                user.password = hash;
-                next();
-            });
-        });
-    } else {
-        return next();
-    }
-});
-
-UserSchema.methods.comparePassword = function(passw, cb) {
-    bcrypt.compare(passw, this.password, function(err, isMatch) {
-        if(err) {
-            return cb(err);
-        }
-        cb(null, isMatch);
-    });
-};
-*/
 const User = mongoose.model("user", UserSchema);
 
 module.exports = User;
